6722a576672bb11de99de3e5eec426bfd0b209e7
이름 <jihoon708@naver.com>
2026-05-08 14:55:36 +0900

    feat: wire lullaby1-4.mp3 samples into LullabyTrack enum

--- app/src/main/java/com/odom/moodlight/data/model/LullabyTrack.kt
+++ app/src/main/java/com/odom/moodlight/data/model/LullabyTrack.kt
@@ -1,7 +1,8 @@
 package com.odom.moodlight.data.model
 
 enum class LullabyTrack(val title: String, val resourceName: String) {
-    BRAHMS("브람스 자장가", "lullaby_brahms"),
-    TWINKLE("반짝반짝 작은별", "lullaby_twinkle"),
-    MOZART("모차르트 자장가", "lullaby_mozart"),
+    TRACK1("브람스 자장가", "lullaby1"),
+    TRACK2("반짝반짝 작은별", "lullaby2"),
+    TRACK3("모차르트 자장가", "lullaby3"),
+    TRACK4("베토벤 엘리제를 위하여", "lullaby4"),
 }
