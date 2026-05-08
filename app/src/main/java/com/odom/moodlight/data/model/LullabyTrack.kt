715ec7ed6fb4d36727ce2dcda189ab107876a9a4
이름 <jihoon708@naver.com>
2026-05-08 13:33:07 +0900

    feat: add LullabyTrack enum for playlist

---
+++ app/src/main/java/com/odom/moodlight/data/model/LullabyTrack.kt
@@ -0,0 +1,7 @@
+package com.odom.moodlight.data.model
+
+enum class LullabyTrack(val title: String, val resourceName: String) {
+    BRAHMS("브람스 자장가", "lullaby_brahms"),
+    TWINKLE("반짝반짝 작은별", "lullaby_twinkle"),
+    MOZART("모차르트 자장가", "lullaby_mozart"),
+}
